f69c0fabef58104e3a03503f2d93f93f84a8b154
John Strack <Braveheart22@GMail.com>
2018-04-09 13:24:21 -0400

    Create a service connection

--- app/src/main/java/com/johnstrack/odometer/MainActivity.java
+++ app/src/main/java/com/johnstrack/odometer/MainActivity.java
@@ -1,9 +1,24 @@
 package com.johnstrack.odometer;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class MainActivity extends Activity {
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
